4a99f002e97b18fc69b7b948f8daa2972d412e4a
munamuncher <130461574+munamuncher@users.noreply.github.com>
2024-04-29 16:25:22 +0900

    owo
    keyBinding need changeing still animation manager is needed to sort the ainimation out broza

--- Assets/Scripts/SkillComponents/SkillScript.cs
+++ Assets/Scripts/SkillComponents/SkillScript.cs
@@ -5,53 +5,56 @@ using UnityEngine;
 public class SkillScript : MonoBehaviour
 {
     public SkillComponent[] skillComponents;
-
-    public KeyCode Skill01 = KeyCode.H;
-    public KeyCode Skill02 = KeyCode.J;
-    public KeyCode Skill03 = KeyCode.K;
-    public KeyCode Skill04 = KeyCode.L;
+    [Header("Skill Inputs")]
+    [SerializeField] private string skill01Input = "Skill01";
+    [SerializeField] private string skill02Input = "Skill02";
+    [SerializeField] private string skill03Input = "Skill03";
+    [SerializeField] private string skill04Input = "Skill04";
 
     private void Start()
     {
-        foreach(SkillComponent component in skillComponents)
-        {
-            component.Activate();
-        }    
+        InitializeSkillComponents();
     }
 
     private void Update()
-    {       
-        if(Input.GetKeyDown(Skill01))
-        {
-            ActivateSkill<Skill01script>(); //mana 10
-        }
-        if (Input.GetKeyDown(Skill02))
-        {
-            ActivateSkill<Skill02script>(); //mana 20으로 너프예정
-        }
-        if (Input.GetKeyDown(Skill03))
-        {
-            ActivateSkill<Skill03script>();   //mana 50으로 너프예정
-        }
-        if (Input.GetKeyDown(Skill04))
-        {
-            ActivateSkill<Skill04script>(); //mana 200으로 너프
-        }
+    {
+        CheckSkillActivation();
     }
 
-    void ActivateSkill<T>() where T : SkillComponent
+    private void InitializeSkillComponents()
     {
-        T[] skillComponents = FindObjectsOfType<T>();
-
-        Debug.Log("Skill components found: " + skillComponents.Length);
-
-        foreach (T component in skillComponents)
+        foreach (SkillComponent component in skillComponents)
         {
-            if (component is T)
-            { 
-                T typedComponent = component as T;
-                typedComponent.Activate();
-            }
+            component.Activate();
+        }
+    }
+    private void CheckSkillActivation()
+    {
+        if (Input.GetButtonDown(skill01Input))
+        {
+            ActivateSkill<Skill01script>();
+        }
+        if (Input.GetButtonDown(skill02Input))
+        {
+            ActivateSkill<Skill02script>();
+        }
+        if (Input.GetButtonDown(skill03Input))
+        {
+            ActivateSkill<Skill03script>();
+        }
+        if (Input.GetButtonDown(skill04Input))
+        {
+            ActivateSkill<Skill04script>();
+        }
+    }
+    // 스킬 사용 계선 필요
+    private void ActivateSkill<T>() where T : SkillComponent
+    {
+        T[] skillComponentsOfType = FindObjectsOfType<T>();
+
+        foreach (T component in skillComponentsOfType)
+        {
+            component.Activate();
         }
     }
 }
